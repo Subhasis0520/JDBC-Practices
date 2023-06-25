f66f7698f4711c6427bd9150d175de4233141be2
Subhasis Ghosh <110963431+Subhasis0520@users.noreply.github.com>
2023-06-25 20:54:17 +0530

    Add files via upload

---
+++ VisitorDataManagment/src/main/java/com/jsp/visitordatamanagment/test/Test.java
@@ -0,0 +1,15 @@
+package com.jsp.visitordatamanagment.test;
+
+
+
+
+import com.jsp.visitordatamanagment.service.VisitorServiceImp;
+
+public class Test {
+
+	public static void main(String[] args) {
+		VisitorServiceImp serviceImp = new VisitorServiceImp();
+		
+		System.out.println(serviceImp);
+	}
+}
---
+++ VisitorDataManagment/src/main/java/com/jsp/visitordatamanagment/util/AppContants.java
@@ -0,0 +1,11 @@
+package com.jsp.visitordatamanagment.util;
+
+public interface AppContants {
+
+	String DRIVER_NAME = "com.mysql.cj.jdbc.Driver";
+	String URL = "jdbc:mysql://localhost:3306/e4";
+	String USER_NAME = "root";
+	String PASSWORD = "Subhasis@123";
+	String SUCESS_MESSAGE = "Opreation Sucessful";
+	String ERROR_MESSAGE = "Something went wrong";
+}
